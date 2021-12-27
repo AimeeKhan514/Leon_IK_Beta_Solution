e7c6fddf1c5019f07da27d45bb2df491370d0750
Aimee Khan <aimeekhan514@gmail.com>
2021-12-27 17:18:38 +0500

    a

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Aimee khan">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/custom.css">
@@ -14,13 +17,260 @@
 </head>
 
 <body>
-<div class="p-3"></div>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand py-0 px-3" href="#">
+                <img src="./assets/images/Beta-Solutions-Logo.svg" alt="" style="height: 92px;" class="img-fluid"
+                    id="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse  bg-dark-custom" id="navbarNav">
+                <ul class="navbar-nav pt_bold text-uppercase">
+                    <li class="nav-item">
+                        <a class="nav-link active-menu" href="index">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about-us">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="faq">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-success-custom" href="javascript:void(0);">Phone +00 0 000 0000</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- NAVBAR -->
+    <!-- NEWS -->
+    <div class="text-dark-custom bg-success-custom text-center py-2">
+        <p class="fs-4 fw-light">We are "Approved Research Providers" for the NZ R&D Tax Incentive</p>
+    </div>
+    <!-- NEWS -->
+    <!-- Image Header -->
+    <div class="col-12 bg-dark-custom2 py-5">
+        <div class="col-lg-6 mx-auto px-3 mb-3">
+            <img src="./assets/images/betasolutions-banner-desktop+(1)-900w.png" alt="" class="img-fluid">
+        </div>
+        <div class="col-lg-12 px-3 mb-3 text-center">
+            <p class="display-4 text-white">We bring your electronic product ideas to life</p>
+            <p class="text-white fs-4 ">Product design consultants in high-tech electronic and electro-mechanical
+                engineering</p>
+        </div>
+    </div>
+    <!-- Image Header -->
+
+    <!-- CONTENT -->
+    <div class="container-fluid p-lg-5 p-3">
+
+        <div class="col-12 mb-4">
+            <p class="text-dark-custom display-6">Beta Solutions is an innovative full-service electronic engineering
+                consultancy, from electronic product design and development to production.</p>
+        </div>
+        <div class="col-12 mb-4">
+            <p class="text-secondary fs-4">Turning new product ideas into reality or ageing products into market winners, we are passionate about innovation with purpose. Beta Solutions is your electronic product design consultancy partner of choice for Electronic Hardware and Embedded Software design;
+                Electro-mechanical design; Production and professional Consultancy advice. With clients throughout New Zealand and internationally, we are set up to successfully manage, design, and deliver your project regardless of the size or scope. Beta Solutions is an Approved Research Provider for the R&D Tax Incentive in New Zealand.</p>
+        </div>
+        <div class="col-12 mb-4">
+            <div class="row">
+                <div class="col-lg-6 mb-3 pe-md-3">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                    <img src="./assets/images/Concept-to-Commercialisation-92129363-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Concept to Commercialisation</span>
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    From product conceptualisation through to a ready-to-manufacture electronic product, we translate market needs into technical
+                                    specs and provide researched based solutions to your problems.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <img src="./assets/images/Specialised-and-Validated-Capabilities-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Specialised and Validated
+                                        Capabilities</span>
+                                </button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    With experience in private sector product development as well as Universities and
+                                    Crown Research Institutes, we have the proven in-house resources required to bring
+                                    your project to life.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingThree">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <img src="./assets/images/Electronic-Research-and-Development-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Electronic Research and Development</span>
+                                </button>
+                            </h2>
+                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                We assist your company to achieve and maintain a competitive advantage in the marketplace by designing your product with the latest emerging technology in mind.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingFour">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    <img src="./assets/images/Rapid-Product-Development-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Rapid Product Development</span>
+                                </button>
+                            </h2>
+                            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                As the development partner for your project, we know how to get your product to the market in the shortest time possible with minimal impact on your day to day business.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingFive">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    <img src="./assets/images/Product-Development-Consulting-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Product Development Consulting</span>
+                                </button>
+                            </h2>
+                            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                Our experienced team of engineers can provide you with specialist consulting services ranging from reliability analysis to more advanced architecture development, product testing, user trials, design reviews, process consulting, architecture assessments as well as cost reduction without compromising quality.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingSix">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                    <img src="./assets/images/Product-Manufacturing-1920w.png" alt=""
+                                        class="img-fluid me-3">
+                                    <span class="fs-4 text-success-custom ">Product Manufacturing</span>
+                                </button>
+                            </h2>
+                            <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                As part of our end to end services we offer our clients low volume in-house production as well as high volume production via our trusted network of local and international suppliers, ensuring on time delivery and quality management.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-3 ps-md-3 d-flex justify-content-center align-items-center">
+                    <video src="./assets/videos/Electronic Design - Product Development Company - Beta Solutions NZ.mp4" controls autoplay muted loop class="img-fluid"></video>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 mb-4 text-center">
+            <a href="services" class="btn-lg btn-success-custom text-decoration-none py-3 px-5">Learn More</a>
+        </div>
+
+    </div>
+    <div class="container-fluid bg-light-custom  p-lg-5 p-3">
+    <div class="col-12 my-4">
+            <p class="text-dark-custom display-6">Industries</p>
+        </div>
+        <div class="col-12 mb-4">
+            <p class="text-dark-custom  fs-4 mb-5">Beta Solutions is a trusted ally to innovative businesses in delivering intelligent and technology optimised, electronic product solutions.</p>
+            <p class="text-dark-custom fs-4">Our wide-ranging experience and knowledge of leading technologies enable us to design electronic products for a wide range of industries. If you don’t see the right category for your project in our industries list here, it is very likely that we can still meet your needs.</p>
+        </div>
+<div class="col-12 mb-4">
+  <div class=" row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
+    Link with href
+  </a>
+  <div class="collapse" id="collapse1">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
+</div>
+<div class="col">
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">
+    Link with href
+  </a>
+  <div class="collapse" id="collapse2">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
+</div>
+  </div>
+
+    </div>
+    <!-- CONTENT -->
 
     <script src="./assets/js/jquery.js"></script>
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
     <script src="./assets/js/custom.js"></script>
     <script>
+    $(window).scroll(function() {
+        $page_scroll = $(document).scrollTop();
+        if ($page_scroll > 100) {
+            $("#navbar").css({
+                "position": "fixed",
+                "padding": "0px 0px",
+                "width": "100%",
+                "transition": "0.5s",
+                "background-color": "#333333"
+            });
+            $("#logo").css({
+                "padding": "10px 20px",
+                "height": "60px",
+                "transition": "0.5s"
+            });
 
+        } else {
+            $("#navbar").css({
+                "position": "static",
+                "padding": "25px 0px",
+                "width": "auto",
+                "transition": "0.5s",
+                "background-color": "white"
+            });
+            $("#logo").css({
+                "padding": "0px 0px",
+                "height": "92px",
+                "transition": "0.5s"
+            });
+        }
+    });
     </script>
 </body>
 
