01f5f57d1016901f95be5631514911b7fc488a24
Aimee Khan <aimeekhan514@gmail.com>
2021-12-28 18:05:45 +0500

    j

--- index.php
+++ index.php
@@ -807,65 +807,112 @@
         </div>
     </div>
     <div class="container-fluid bg-dark-custom p-lg-5 p-3">
-        <div class="col-12">
-            <div class="row">
-                <div class="col-md-3 mb-3">
-                    <div class="col-12 mb-2">
-                        <a href="about" class="h3 fw-bold text-decoration-none text-success-custom">About</a>
-                    </div>
-                    <div class="col-12">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Culture</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Facilities</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Team</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Faqs</a>
-                            </li>
-                        </ul>
-                    </div>
+        <div class="row py-3">
+            <div class="col-md-5">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <div class="col-12 mb-2">
+                            <a href="about" class="h3 fw-bold text-decoration-none text-success-custom">About</a>
+                        </div>
+                        <div class="col-12">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Culture</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Facilities</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Faqs</a>
+                                </li>
+                            </ul>
+                        </div>
 
-                </div>
-                <div class="col-md-3 mb-3">
-                    <div class="col-12 mb-2">
-                        <a href="about" class="h3 fw-bold text-decoration-none text-success-custom">Services</a>
                     </div>
-                    <div class="col-12">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Electronic Product Design Consulting</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Embedded Software & Firmware Development</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Electronics & PCB Design</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">PCB & Electronics Prototyping</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">PCB & Electronic Manufacturing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Process</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Capabilities</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link px-0 py-1 text-white" href="#">Technology</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <div class="col-md-6 mb-3">
+                        <div class="col-12 mb-2">
+                            <a href="about" class="h3 fw-bold text-decoration-none text-success-custom">Services</a>
+                        </div>
+                        <div class="col-12">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Electronic Product Design
+                                        Consulting</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Embedded Software & Firmware
+                                        Development</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Electronics & PCB Design</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">PCB & Electronics Prototyping</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">PCB & Electronic Manufacturing</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Process</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Capabilities</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link px-0 py-1 text-white" href="#">Technology</a>
+                                </li>
+                            </ul>
+                        </div>
 
+                    </div>
                 </div>
             </div>
+            <div class="col-md-5 ms-auto">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <div class="col-12 mb-2">
+                            <a href="about" class="h3 fw-bold text-decoration-none text-success-custom">Follow Us</a>
+                        </div>
+                        <div class="col-12">
+                            <a class="text-white text-decoration-none fs-3" href="#">
+                                <span class="fab fa-linkedin-in"></span>
+                            </a>
+                            <a class="text-white text-decoration-none fs-3" href="#">
+                                <span class="fab fa-facebook-f"></span>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <div class="col-12 mb-4">
+                            <a href="contact"
+                                class="btn-success-custom fs-4 btn-lg  text-decoration-none py-3 px-5">Free
+                                Consultation</a>
+                        </div>
+                        <div class="col-12 text-center">
+                            <p class="fs-5 text-white"><span class="text-success-custom">IT'S ON US,</span> JUST
+                                CALL<br>Phone <span class="text-success-custom">+64 6 280 2830</span></p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <ul class="nav justify-content-center fs-5">
+                <li class="nav-item">
+                    <a class="nav-link  text-white text-decoration-underline" href="#">Privacy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white text-decoration-underline" href="#">SiteMap</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#"> © <?php echo date("Y")?></a>
+                </li>
+            </ul>
         </div>
     </div>
     <!-- FOOTER SECTION -->
